56542e78ef6f8606e86b4115750c09c44aa315a5
Frances Morales Velilla <frances0688@gmail.com>
2017-08-26 19:39:05 +0200

    Create function to compare arrays

--- mastermind_logic.js
+++ mastermind_logic.js
@@ -1,17 +1,38 @@
-var colorsArray = ["red", "blue", "yellow", "green", "orange", "purple"];
+var colors = ["red", "blue", "yellow", "green", "orange", "purple"];
 
-var difficultyLevel;
+var difficultyLevel = 4;
 
-var userArray = [];
+var masterCode = [];
 
-function getRandomColor(array) {
-	return Math.floor(Math.random() * array.length);
+var userChoices = [];
+
+$(document).ready(function() {
+	masterCodeGenerator(difficultyLevel, colors);
+});
+
+function getRandomColor(items) {
+	return Math.floor(Math.random() * items.length);
 }
 
-function masterCodeGenerator(difficultyLevel, array) {
-	var masterCode = [];
+function masterCodeGenerator(difficultyLevel, items) {
+	masterCode = [];
 	for (i = 0; i < difficultyLevel; i++) {
-		code.push(array[getRandomColor(array)]);
+		masterCode.push(items[getRandomColor(items)]);
 	}
-	return masterCode;
+}
+
+function compareCodes(masterCode, userChoices) {
+	var pegs = [];
+	var masterIndex;
+	for (i = 0; i < difficultyLevel; i++) {
+		masterIndex = _.indexOf(masterCode, userChoices[i]);
+		if (masterIndex == i) {
+			pegs.push("blackPeg");
+		} else if (masterIndex > -1) {
+			pegs.push("whitePeg");
+		} else {
+			pegs.push(null);
+		}
+	}
+	return _.shuffle(pegs);
 }
