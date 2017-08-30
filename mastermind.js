91773b058fb1052f90f52122b43e872bb86f10b1
Frances Morales Velilla <frances0688@gmail.com>
2017-08-30 19:38:14 +0200

    Create js function to add choice images on board

--- mastermind.js
+++ mastermind.js
@@ -1,14 +1,16 @@
-//Expands game board according to difficulty level
+// Variables
 var insertPegFirstRow = true;
+var checkBtn = document.getElementById("checkBtn");
+var attempts = 0;
+var colorPicked;
 
+//Expands game board according to difficulty level
 $(".difficulty > button").click(function(event) {
 	var futureLevel = parseInt(event.target.innerHTML);
 	var newColumns = futureLevel - difficultyLevel;
 	if (newColumns >= 0) {
 		for (i = 0; i < newColumns; i++) {
-			$(
-				"<td><img class = 'space' src='img/black circle.png'></td>"
-			).insertAfter(
+			$("<td class='space'></td>").insertAfter(
 				"#main-game > tbody > tr > td:nth-child(" + difficultyLevel + ")"
 			);
 			if (insertPegFirstRow) {
@@ -50,4 +52,40 @@ $(".last-level-btn").click(function() {
 
 $(".default-level-btn").click(function() {
 	$(".next-level, .last-level").css("display", "none");
+});
+
+//Game over function
+$("#checkBtn").on("click", function() {
+	attempts += 1;
+	if (attempts >= 10) {
+		if (confirm("GAME OVER! Want to play again?") == true) {
+			window.location.reload();
+			return;
+		} else {
+			return;
+		}
+	}
+});
+
+// var choice = $(".choices").each(function() {
+// 	$(this).click(function(e) {
+// 		userChoices.push(colors[$(this).index()]);
+// 		console.log(userChoices);
+// 	});
+// });
+
+var choice = $(".choices").each(function() {
+	$(this).click(function() {
+		var index = [$(this).index()];
+		colorPicked = colors[index];
+		console.log(colors[index]);
+	});
+});
+
+$("td").on("click", function() {
+	var element = $(this);
+	var position = element.index();
+	element.addClass("filled " + colorPicked);
+	userChoices[position] = colorPicked;
+	console.log(userChoices);
 });
