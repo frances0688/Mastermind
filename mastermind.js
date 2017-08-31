ba981a14565b22ba15ff557fc9f9c5d2caeee896
Frances Morales Velilla <frances0688@gmail.com>
2017-08-31 18:55:45 +0200

    Create function to show pegs in correct space

--- mastermind.js
+++ mastermind.js
@@ -1,8 +1,10 @@
 // Variables
 var insertPegFirstRow = true;
 var checkBtn = document.getElementById("checkBtn");
-var attempts = 0;
+var attempts = 1;
 var colorPicked;
+$(".space").addClass("blocked");
+$('tr[row="' + attempts + '"] .space').removeClass("blocked");
 
 //Expands game board according to difficulty level
 $(".difficulty > button").click(function(event) {
@@ -61,39 +63,118 @@ var choice = $(".choices").each(function() {
 $("td").on("click", function() {
 	var element = $(this);
 	var position = element.index();
-	element.addClass("filled " + colorPicked);
-	userChoices[position] = colorPicked;
-	if (userChoices.length == difficultyLevel) {
-		document.getElementById("checkBtn").disabled = false;
-		$("#checkBtn").click(function() {
-			var pegsArray = compareCodes(masterCode, userChoices);
-			pegsArray.forEach(function(pegColor) {
-				if (pegColor === "blackPeg") {
-					$(".pegspace")
-						.css("background-image", "none")
-						.addClass("pegColorBlack");
-				} else if (pegColor === "whitePeg") {
-					$(".pegspace")
-						.css("background-image", "none")
-						.addClass("pegColorWhite");
-				} else {
-					return;
-				}
-			});
-		});
+	var row = element.parent();
+
+	if (checkRigthRow(row.attr("row"))) {
+		element.addClass("filled " + colorPicked);
+		userChoices[position] = colorPicked;
+		if (userChoices.length == difficultyLevel) {
+			document.getElementById("checkBtn").disabled = false;
+		}
+	} else {
+		return;
 	}
 });
 
-//Game over function
-$("#checkBtn").on("click", function() {
-	attempts += 1;
-	compareCodes(masterCode, userChoices);
-	if (attempts >= 10) {
+function checkRigthRow(rowNumber) {
+	if (parseInt(rowNumber) === attempts) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+$("#checkBtn").click(function() {
+	if (!gameOver()) {
+		// Update current row
+		$('tr[row="' + attempts + '"] .space').addClass("blocked");
+		attempts++;
+		$('tr[row="' + attempts + '"] .space').removeClass("blocked");
+
+		// show Pegs
+		drawPegs();
+	}
+});
+
+function drawPegs() {
+	var pegsArray = compareCodes(masterCode, userChoices);
+	console.log(pegsArray);
+	var pegSelector;
+	var currentPegsRow = attempts - 1;
+	var i;
+	var i2 = 0;
+	var blockname;
+	pegsArray.forEach(function(pegColor, index) {
+		i = index + 1;
+		if (difficultyLevel % 2 === 0) {
+			console.log("im even !!!");
+			if (i <= difficultyLevel / 2) {
+				blockname = ".firstRowPeg";
+				pegSelector = $(
+					'tr[row="' +
+						currentPegsRow +
+						'"] ' +
+						blockname +
+						" .pegspace:nth-child(" +
+						i +
+						")"
+				);
+			} else {
+				i2++;
+				blockname = ".secondRowPeg";
+				pegSelector = $(
+					'tr[row="' +
+						currentPegsRow +
+						'"] ' +
+						blockname +
+						" .pegspace:nth-child(" +
+						i2 +
+						")"
+				);
+			}
+		}
+
+		console.log(pegSelector);
+		if (pegColor === "blackPeg") {
+			pegSelector.css("background-image", "none").addClass("pegColorBlack");
+		} else if (pegColor === "whitePeg") {
+			pegSelector.css("background-image", "none").addClass("pegColorWhite");
+		} else {
+			return;
+		}
+	});
+}
+
+function gameOver() {
+	if (attempts > 10) {
 		if (confirm("GAME OVER! Want to play again?") == true) {
 			window.location.reload();
 			return;
 		} else {
 			return;
 		}
+	} else {
+		return false;
 	}
+}
+
+// MODE BUTTONS
+// CLASSIC
+$("#classic-btn").on("click", function() {
+	window.location.reload();
+});
+
+// NUMBERS
+$("#numbers-btn").on("click", function() {});
+
+// CATS!
+$("#cats-btn").on("click", function() {
+	$(".classic").css("background-image", "url('img/grass.jpg')");
+	$(".game-board").css("background-image", "url('img/fur.jpg')");
+	$("html, .difficulty>button, #checkBtn").css("font-family", "Poiret One");
+	$("html, .difficulty>button, #checkBtn").css("font-size", "35px");
+	$(".choices").css("background-image", "url('img/cats-spritesheet.png')");
+	$(".choices, .space").css("height", "60px");
+	$(".choices, .space").css("width", "60px");
+	$(".choices, .space").css("border-radius", 0);
 });
